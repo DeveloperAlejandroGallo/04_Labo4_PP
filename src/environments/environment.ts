fbe84726ea70d2a3be399af030efc1231a529ef9
Alejandro Gallo <alegallo@gmail.com>
2020-10-15 22:02:35 -0300

    algo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true
+  production: false,
+  urlPaises: 'https://restcountries.eu/rest/v2/region/americas',
+  jugadores: 'https://api.mocki.io/v1/75aa447e',
+  firebaseConfig : {
+    apiKey: "AIzaSyBCXgFPybYchlOgC_XMPUKoCiFcvYcZYZY",
+    authDomain: "labo4-aleg.firebaseapp.com",
+    databaseURL: "https://labo4-aleg.firebaseio.com",
+    projectId: "labo4-aleg",
+    storageBucket: "labo4-aleg.appspot.com",
+    messagingSenderId: "621586958811",
+    appId: "1:621586958811:web:461d4875ba6fb80a34fffe"
+  }
 };
 
 /*
